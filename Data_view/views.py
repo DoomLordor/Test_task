545febd82bfdc62c1459b522d1900d88672c4814
DoomLordor <49496875+DoomLordor@users.noreply.github.com>
2020-10-13 21:45:26 +0300

    Добавление просмотра данных

--- Data_view/views.py
+++ Data_view/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+from .models import Data
+
+class DataView(View):
+    """Просмотр сводных данных"""
+    def get(self, request):
+        data = Data.objects.all()
+        return render(request, 'index.html', {'data_list': data})
