4343b4adfa456244859605e3859fe8576820bbb5
DoomLordor <49496875+DoomLordor@users.noreply.github.com>
2020-10-27 22:24:58 +0300

    Добавление классов для работы с данными

--- Data_view/views.py
+++ Data_view/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
-from django.views.generic.base import View
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
-from .models import Data
+from .models import Data, HeadByBK
+from .serializers import DataSerializers, HeadByBKSerializers
+from .service import PaginationData, DataFilter, DataPermissions, HeadByBKFilter, HeadByBKPermissions
 
-class DataView(View):
-    """Просмотр сводных данных"""
-    def get(self, request):
-        data = Data.objects.all()
-        return render(request, 'index.html', {'data_list': data})
+
+class DataRest(viewsets.ModelViewSet):
+    queryset = Data.objects.all()
+    serializer_class = DataSerializers
+    pagination_class = PaginationData
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = DataFilter
+    permission_classes = (DataPermissions, )
+
+
+class HeadByBKRest(viewsets.ModelViewSet):
+    queryset = HeadByBK.objects.all()
+    serializer_class = HeadByBKSerializers
+    pagination_class = PaginationData
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = HeadByBKFilter
+    permission_classes = (HeadByBKPermissions,)
