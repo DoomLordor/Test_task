34c8c9360cee462b65934db746a4e021a3276d46
DoomLordor <49496875+DoomLordor@users.noreply.github.com>
2020-10-27 22:01:07 +0300

    Добавление URL для просмотра данных

--- Data_view/urls.py
+++ Data_view/urls.py
@@ -3,5 +3,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.DataView.as_view()),
+    path('Data/', views.DataRest.as_view({'get': 'list', 'post': 'create'})),
+
+    path('Data/<int:pk>/', views.DataRest.as_view({'get': 'retrieve',
+                                                   'put': 'update',
+                                                   'patch': 'partial_update',
+                                                   'delete': 'destroy'})),
+
+    path('HeadByBK/', views.HeadByBKRest.as_view({'get': 'list', 'post': 'create'})),
+
+    path('HeadByBK/<int:pk>/', views.HeadByBKRest.as_view({'get': 'retrieve',
+                                                           'put': 'update',
+                                                           'patch': 'partial_update',
+                                                           'delete': 'destroy'})),
 ]
