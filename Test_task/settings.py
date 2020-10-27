543c0f27111c895503090b43630537bfe6be7cf9
DoomLordor <49496875+DoomLordor@users.noreply.github.com>
2020-10-27 20:14:23 +0300

    Добавление настроек для фильров и пагинации по-умолчанию

--- Test_task/settings.py
+++ Test_task/settings.py
@@ -75,6 +75,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Test_task.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
