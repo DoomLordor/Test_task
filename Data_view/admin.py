6dabc244dcde937430ef4bff8f3127d0e32f4d13
DoomLordor <49496875+DoomLordor@users.noreply.github.com>
2020-10-30 16:37:35 +0300

    Регистрация моделей в панели администратора

--- Data_view/admin.py
+++ Data_view/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Data, HeadByBK
+from .models import (Data, HeadByBK, TypeInstitutions,
+                     TypeOrganizations, StatusEGRUL,
+                     StatusRYBPNYBP, IndustrySpecificTyping, BudgetLevel)
 
 
 admin.site.register(Data)
 admin.site.register(HeadByBK)
+admin.site.register(TypeInstitutions)
+admin.site.register(TypeOrganizations)
+admin.site.register(StatusEGRUL)
+admin.site.register(StatusRYBPNYBP)
+admin.site.register(IndustrySpecificTyping)
+admin.site.register(BudgetLevel)
+
