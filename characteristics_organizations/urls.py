10d79d7de03407ccfe193980c6b5f2e6a944097d
DoomLordor <49496875+DoomLordor@users.noreply.github.com>
2021-01-07 14:37:56 +0300

    Переименнование

--- characteristics_organizations/urls.py
+++ characteristics_organizations/urls.py
@@ -3,29 +3,29 @@ from django.urls import path
 from . import views
 
 LIST_VIEW_METHODS = {'get': 'list', 'post': 'create'}
-object_OBJECT_VIEW_METHODS = {'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}
+OBJECT_VIEW_METHODS = {'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}
 
 urlpatterns = [
     path('CharacteristicsOrganization/', views.CharacteristicsOrganizationRest.as_view(LIST_VIEW_METHODS)),
     path('CharacteristicsOrganization/<int:pk>/',
-         views.CharacteristicsOrganizationRest.as_view(object_OBJECT_VIEW_METHODS)),
+         views.CharacteristicsOrganizationRest.as_view(OBJECT_VIEW_METHODS)),
 
     path('HeadByBK/', views.HeadByBKRest.as_view(LIST_VIEW_METHODS)),
-    path('HeadByBK/<int:pk>/', views.HeadByBKRest.as_view(object_OBJECT_VIEW_METHODS)),
+    path('HeadByBK/<int:pk>/', views.HeadByBKRest.as_view(OBJECT_VIEW_METHODS)),
 
     path('TypeInstitutions/', views.TypeInstitutionRest.as_view(LIST_VIEW_METHODS)),
-    path('TypeInstitutions/<int:pk>/', views.TypeInstitutionRest.as_view(object_OBJECT_VIEW_METHODS)),
+    path('TypeInstitutions/<int:pk>/', views.TypeInstitutionRest.as_view(OBJECT_VIEW_METHODS)),
 
     path('TypeOrganizations/', views.TypeOrganizationRest.as_view(LIST_VIEW_METHODS)),
-    path('TypeOrganizations/<int:pk>/', views.TypeOrganizationRest.as_view(object_OBJECT_VIEW_METHODS)),
+    path('TypeOrganizations/<int:pk>/', views.TypeOrganizationRest.as_view(OBJECT_VIEW_METHODS)),
 
     path('StatusEGRUL/', views.StatusEGRULRest.as_view(LIST_VIEW_METHODS)),
-    path('StatusEGRUL/<int:pk>/', views.StatusEGRULRest.as_view(object_OBJECT_VIEW_METHODS)),
+    path('StatusEGRUL/<int:pk>/', views.StatusEGRULRest.as_view(OBJECT_VIEW_METHODS)),
 
     path('StatusRYBPNYBP/', views.StatusRYBPNYBPRest.as_view(LIST_VIEW_METHODS)),
-    path('StatusRYBPNYBP/<int:pk>/', views.StatusRYBPNYBPRest.as_view(object_OBJECT_VIEW_METHODS)),
+    path('StatusRYBPNYBP/<int:pk>/', views.StatusRYBPNYBPRest.as_view(OBJECT_VIEW_METHODS)),
 
     path('IndustrySpecificTyping/', views.IndustrySpecificTypingRest.as_view(LIST_VIEW_METHODS)),
-    path('IndustrySpecificTyping/<int:pk>/', views.IndustrySpecificTypingRest.as_view(object_OBJECT_VIEW_METHODS)),
+    path('IndustrySpecificTyping/<int:pk>/', views.IndustrySpecificTypingRest.as_view(OBJECT_VIEW_METHODS)),
 
 ]
